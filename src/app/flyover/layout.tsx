4f0fca468847d80ac88d263cc2fdc0ab682981bd
rahulsya <rahulsyaban@gmail.com>
2023-08-17 22:23:57 +0700

    feat: add check session login

--- src/app/flyover/layout.tsx
+++ src/app/flyover/layout.tsx
@@ -1,14 +1,30 @@
+"use client";
 import Navigation from "@/components/Navigation";
-import React from "react";
+import { useAuthContext } from "@/firebase/AuthContext";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 type Iprops = {
   children: React.ReactNode;
 };
 function FlyoverInvoiceLayout({ children }: Iprops) {
+  const { user, authLoading } = useAuthContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!authLoading && !user) {
+      router.push("/login");
+    }
+  }, [authLoading, user]);
+
   return (
     <div className="w-full bg-gray-400">
       <Navigation />
-      <div className="mx-auto px-12 pb-12">{children}</div>
+      {!user ? (
+        "Check login status..."
+      ) : (
+        <div className="mx-auto px-12 pb-12">{children}</div>
+      )}
     </div>
   );
 }
