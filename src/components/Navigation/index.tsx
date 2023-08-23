da6a3a5e86ac249cca4bf27d8b918d028abf9f61
rahulsya <rahulsyaban@gmail.com>
2023-08-23 21:51:05 +0700

    feat(auth): add action logout

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,16 +1,30 @@
+import { useAuthContext } from "@/firebase/AuthContext";
+import { logout } from "@/firebase/auth";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function Navigation() {
+  const { user } = useAuthContext();
+  const router = useRouter();
+
+  const onLogout = async () => {
+    const signout = await logout();
+    if (signout) {
+      router.push("/login");
+    }
+  };
   return (
     <div className="flex justify-between items-center bg-gray-900 text-white px-8 py-4 mb-4 drop-shadow-xl">
       <div className="text-lg font-semibold">Invoices</div>
       <div className="flex ">
-        {/* <div className="mx-3 px-4 py-2 bg-blue-800 rounded-lg cursor-pointer">
-          Preview
-        </div>
-        <div className="px-4 py-2 bg-white rounded-lg text-blue-800 cursor-pointer">
-          Download
-        </div> */}
+        {user && (
+          <div
+            onClick={() => onLogout()}
+            className="px-4 py-2 bg-white rounded-lg text-blue-800 cursor-pointer"
+          >
+            Logout
+          </div>
+        )}
       </div>
     </div>
   );
