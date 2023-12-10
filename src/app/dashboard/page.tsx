a8a5b991acc9ae1f9c1ad750520de9c256c9db7b
rahulsya <rahulsyaban@gmail.com>
2023-12-10 19:19:17 +0700

    feat(dashboard): add title & toolbar action

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,18 @@
+import Button from "@/components/button";
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div className="flex flex-col gap-2 px-3">
+        <div className="text-xl font-semibold">Dashboard.</div>
+        <div className="mb-4 flex flex-row flex-wrap gap-4 rounded-lg border p-4 shadow-md">
+          <Button title="Filter Data" />
+          <Button type="warning" title="Download report" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
