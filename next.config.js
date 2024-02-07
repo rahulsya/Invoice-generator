dc9d708492f55d2b6478455ae7957b32cf770a52
rahulsya <rahulsyaban@gmail.com>
2024-02-07 23:40:09 +0700

    chore : add config redirects default path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
