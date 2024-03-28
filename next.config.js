043bebd9f1afafee4926e414807cb1659313b544
rahulsya <rahulsyaban@gmail.com>
2024-03-29 02:14:50 +0700

    fix : add domains img url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: ["firebasestorage.googleapis.com"],
+    domains: ["firebasestorage.googleapis.com"],
     remotePatterns: [
       {
         protocol: "https",
