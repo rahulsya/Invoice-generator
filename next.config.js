2c40619a915f4949a434cf9fc58999cfb5e6c591
rahulsya <rahulsyaban@gmail.com>
2024-03-30 21:00:37 +0700

    fix(setting) : img optimalization

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: ["firebasestorage.googleapis.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/v0/b/flyoversticker-e4fea.appspot.com/o/**",
-      },
-    ],
+    domains: ["firebasestorage.googleapis.com"],
+    output: "standalone",
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "firebasestorage.googleapis.com",
+    //     port: "",
+    //     pathname: "/v0/b/flyoversticker-e4fea.appspot.com/o/**",
+    //   },
+    // ],
   },
   async redirects() {
     return [
